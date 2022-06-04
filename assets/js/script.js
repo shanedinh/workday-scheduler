a0103fc04543b4a1c3b4cc6dc75f220a53b1599c
Shane Dinh <shane.dinh5@gmail.com>
2022-06-04 16:44:46 -0500

    show state of block of time by color, ability ot save items to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,39 @@
 var today = moment().format('LL');
 $("#currentDay").text(today);
 
-$(".saveBtn").on('click', function() {
-    var textInfo = $(this).siblings
+$(".saveBtn").on("click", function() {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
 })
+
+function timeCompare() {
+    var currentTime = moment().hour();
+    console.log(currentTime)
+
+    $(".time-block").each(function() {
+        var blockTime = parseInt($(this).attr("id"));
+        console.log(blockTime);
+
+        if (blockTime < currentTime) {
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+        else if (currentTime === blockTime) {
+            $(this).addClass("present");
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).addClass("future");
+            $(this).removeClass("past");
+        }
+    })
+
+}
+
+
+timeCompare();
