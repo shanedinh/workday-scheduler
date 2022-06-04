f23bd75d566f8b2c7b8597cdc3023b47bf6babc4
Shane Dinh <shane.dinh5@gmail.com>
2022-06-04 17:03:53 -0500

    added comments and small tweaks to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,23 @@
 var today = moment().format('LL');
 $("#currentDay").text(today);
 
+// function to save, event listener for save button
 $(".saveBtn").on("click", function() {
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
-
+    // save text to local storage
     localStorage.setItem(time, text);
 })
 
+// compary current time to block of time - show status by color
 function timeCompare() {
     var currentTime = moment().hour();
-    console.log(currentTime)
 
+    // loop over time blocks
     $(".time-block").each(function() {
         var blockTime = parseInt($(this).attr("id"));
-        console.log(blockTime);
 
+        // comparing the times, adding classes for color indicators
         if (blockTime < currentTime) {
             $(this).removeClass("present");
             $(this).removeClass("future");
@@ -36,7 +38,7 @@ function timeCompare() {
 
 }
 
-
+// retrieve item from local storage
 $("#8 .description").val(localStorage.getItem(8));
 $("#9 .description").val(localStorage.getItem(9));
 $("#10 .description").val(localStorage.getItem(10));
